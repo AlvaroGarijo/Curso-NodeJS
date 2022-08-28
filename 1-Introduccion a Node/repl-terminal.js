af967a1c4567ef7fe1b8e0bb8d312c6c2999bfe3
AlvaroGarijo <agarijo@padelmanager.com>
2022-08-29 00:14:53 +0200

    organización de carpetas

---
+++ 1-Introduccion a Node/repl-terminal.js
@@ -0,0 +1,14 @@
+/*
+* Curso de Node.js 
+*/
+
+function saludar(nombre) {
+    return `Hola, ${nombre}.`;
+  }
+  
+  saludar("Álvaro");
+
+  //Línea de comando: node
+  //Linea de comando: .editar
+  //Para comprobar en terminal, añadir archivo despues de node, ejemplo: node repl-terminal.js
+  //Hola, Álvaro.
